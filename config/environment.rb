6e131e4b04c1184f66223948fec04f972ec866f0
Henning Athmer <henning.athmer@gmail.com>
2019-01-27 13:02:14 +0000

    Added code comments

--- config/environment.rb
+++ config/environment.rb
@@ -3,14 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-ActionMailer::Base.stmp_settings = {
-  :port           => ENV['Mailgun_SMTP_PORT'],
-  :address        => ENV['Mailgun_SMTP_SERVER'],
-  :user_name      => ENV['Mailgun_SMTP_LOGIN'], 
-  :password       => ENV['Mailgun_SMTP_PASSWORD'], 
-  :domain         => 'rocky-coast-72930.herokuapp.com',
-  :authentication => :plain, 
-}
-ActionMailer::Base.delivery_method = :smtp
-
